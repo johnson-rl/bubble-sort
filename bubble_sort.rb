2f1704dab540e6bf26ba140c0c0f6a010ec8ec21
johnson-rl <johnson.rl@gmail.com>
2017-02-02 18:40:03 -0800

    bubbles are rising

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,18 @@
 def bubble_sort(arr)
-  # code in here
+  swap = true
+  n = (arr.length)-2
+  i = 0
+  while swap == true
+    swap = false
+    for i in 0..n do
+      if arr[i] > arr[i+1]
+        x = arr.slice!((i), 2)
+        x.reverse!
+        arr.insert((i), x[0], x[1])
+        swap = true
+      end
+    end
+    n -= 1
+  end
+  arr
 end
